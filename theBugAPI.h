81bcf70d6fd9061258dff7f88eff5931b9b11758
YagoRegis <yyagoregess37@gmail.com>
2015-03-28 17:29:46 -0300

    Modificando função de move, calculo utilizando distância agora

--- theBugAPI.h
+++ theBugAPI.h
@@ -1,8 +1,14 @@
+#define PI 3.1416
+
+#define WHEEL_DIAMETER 4.32
+
+
 sub turn(){
     RotateMotorEx(OUT_BC, 75, 360, 100, true, true);
 }
 
-sub move(int power, long angle){
+sub move(int power, int distance){
+	int angle = (distance/(PI*WHEEL_DIAMETER))*360;
     RotateMotor(OUT_BC, power, angle);
 }
 
