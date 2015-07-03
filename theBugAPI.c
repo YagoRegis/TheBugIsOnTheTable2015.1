3f6f3898d9515adbc279e121f909f4091029bbf5
JonathanRufino <jonathan.rufinotkd@gmail.com>
2015-07-03 00:01:31 -0300

    Adicionando descrição da API e da música

--- theBugAPI.c
+++ theBugAPI.c
@@ -4,7 +4,6 @@
 #define WHEEL_DIAMETER 4.32
 #define CAR_RADIUS 6.65
 
-
 #define RIGHT -1
 #define LEFT 1
 
@@ -19,20 +18,26 @@
 #define COMPLETE_ROTATION 360
 #define HALF_ROTATION 180
 
+sub turn(int angle, int power, int direction ) {
+	//Measured in centimeter
+	int car_arc = ( angle * PI * CAR_RADIUS ) /
+		HALF_ROTATION;
 
+	// Measured in centimeter
+	int amount_turns = (HALF_ROTATION * car_arc) /
+		(PI * WHEEL_RADIUS);
 
-sub turn(int angle, int power, int direction ){
-  int car_arc = ( angle * PI * CAR_RADIUS )/ HALF_ROTATION; // Measured in centimeter
-  int amount_turns = (HALF_ROTATION * car_arc) /(PI * WHEEL_RADIUS); // Measured in centimeter
-
-  RotateMotorExPID(OUT_BC, power, amount_turns, 100 * direction,
-                    true, true, 40, 40, 90);
+	RotateMotorExPID(OUT_BC, power, amount_turns,
+		100 * direction, true, true, 40, 40, 90);
 }
-sub move(int distance, int power, int direction){
-  int angle = (distance * COMPLETE_ROTATION * direction)/(PI * WHEEL_DIAMETER);
-  RotateMotorEx(OUT_BC, power, angle, 0, true, true);
+
+sub move(int distance, int power, int direction) {
+	int angle = (distance * COMPLETE_ROTATION * direction) /
+		(PI * WHEEL_DIAMETER);
+
+	RotateMotorEx(OUT_BC, power, angle, 0, true, true);
 }
 
-sub claw_control(int angle, int power, int direction){
-  RotateMotor(OUT_A, power, angle * direction);
+sub claw_control(int angle, int power, int direction) {
+	RotateMotor(OUT_A, power, angle * direction);
 }
